53d95e89693612319175be687a6cddbb02f86da0
Andrzej <xurten@gmail.com>
2017-01-20 12:44:09 +0100

    Added model Person

---
+++ LinqProject/LinqProject/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LinqProject
+{
+    public class Person
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string EmailAddress { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public Person()
+        {
+
+        }
+
+        public Person(Guid id, string firstName, string lastName, int age, string emailAddress, DateTime birthdate)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            EmailAddress = emailAddress;
+            BirthDate = birthdate;
+        }
+    }
+}
