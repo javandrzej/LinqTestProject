95bb31919a93b6854e74a9f236e08cb054d8c42d
Andrzej <xurten@gmail.com>
2017-01-20 12:18:38 +0100

    Create LinqProject library and test project

---
+++ LinqProject/LinqProject/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqProject
+{
+    public class Class1
+    {
+    }
+}
