1102cf26403352f740cdbd579d6c3085e1eb6ff1
Andrzej <xurten@gmail.com>
2017-01-20 13:27:13 +0100

    Creating persons and added test

--- LinqProject/LinqProject/Utils/DataGenerator.cs
+++ LinqProject/LinqProject/Utils/DataGenerator.cs
@@ -1,6 +1,14 @@
-﻿namespace LinqProject.Utils
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinqProject.Utils
 {
-    public static class DataGenerator
+    public class DataGenerator : IGenarator
     {
+        public IEnumerable<Person> CreatePersons(int count)
+        {
+            return Enumerable.Range(1, count).Select(p => new Person(Guid.NewGuid(), "firstname" + p, "lastname" + p, 20 + p, "myemail" + p + p, DateTime.Now.Add(new TimeSpan(p, 0, 0, 0))));
+        }
     }
 }
---
+++ LinqProject/LinqProject/Utils/IGenarator.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace LinqProject.Utils
+{
+    public interface IGenarator
+    {
+        IEnumerable<Person> CreatePersons(int count);
+    }
+}
